fb75ececf1d4a07e973ed69bc27b67078e195a49
Krzysztof Kubanek <krzysztof.kubanek@gmail.com>
2022-07-15 14:40:14 +0200

    feat!: implements additional features of AMQP library, breaking change on ConsumerInterface

--- src/ConsumerInterface.php
+++ src/ConsumerInterface.php
@@ -4,5 +4,7 @@ namespace AgDevelop\RabbitMqProducerConsumer;
 
 interface ConsumerInterface
 {
+    public function init(): void;
+
     public function consume(): void;
 }
--- src/RabbitMqConsumer.php
+++ src/RabbitMqConsumer.php
@@ -19,19 +19,38 @@ class RabbitMqConsumer implements ConsumerInterface
         private MessageHandlerResolverInterface $handlerResolver,
         private ?LoggerInterface $logger,
         private string $queueName = '',
+        private string $consumerTag = '',
+        private bool $noLocal = false,
+        private bool $noAck = false,
+        private bool $exclusive = false,
+        private bool $noWait = false,
+        private bool $nonBlocking = true,
+        private int $timeout = 0,
     ) {
     }
 
-    /**
-     * @throws HandlerException, \Exception
-     */
-    public function consume(): void
+    public function init(): void
     {
+        /*
+            queue: Queue from where to get the messages
+            consumer_tag: Consumer identifier
+            no_local: Don't receive messages published by this consumer.
+            no_ack: If set to true, automatic acknowledgement mode will be used by this consumer. See https://www.rabbitmq.com/confirms.html for details.
+            exclusive: Request exclusive consumer access, meaning only this consumer can access the queue
+            nowait:
+            callback: A PHP Callback
+        */
+
         $this->channel->basic_consume(
             queue: $this->queueName,
-            callback: function (AMQPMessage $msg) {
+            consumer_tag: $this->consumerTag,
+            no_local: $this->noLocal,
+            no_ack: $this->noAck,
+            exclusive: $this->exclusive,
+            nowait: $this->noWait,
+            callback: function (AMQPMessage $message) {
                 try {
-                    $deserializer = $this->deserializerBuilder->build($msg->body);
+                    $deserializer = $this->deserializerBuilder->build($message->body);
                     $object = $deserializer->deserialize()->getObject();
                     $envelope = $deserializer->getEnvelope();
                     $this->logger?->info(sprintf('Message %s@%s received, sent at %s ',
@@ -42,14 +61,22 @@ class RabbitMqConsumer implements ConsumerInterface
                     $handler = $this->handlerResolver->getHandlerFor($object);
                     $handler->handle($object);
                     $this->logger?->info('Message handled - sending ACK to Queue Manager');
-                    $msg->ack();
+                    $message->ack();
                 } catch (UnableToDetermineHandlerException $exception) {
                     /* only unknown handler exceptions are to be handled here since we need to send ACK if no
                        matching handler is provided; all other exceptions should be passed out of this method */
                     $this->logger?->warning('Could not determine handler - sending ACK to Queue Manager');
-                    $msg->ack();
+                    $message->ack();
                 }
             },
         );
+    }
+
+    /**
+     * @throws HandlerException, \Exception
+     */
+    public function consume(): void
+    {
+        $this->channel->wait(null, $this->nonBlocking, $this->timeout);
     }
 }
